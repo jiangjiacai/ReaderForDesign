d59330bd60529dcea56d83d527d5b222a9d749da
admin <ly5609101>
2017-03-30 11:58:42 +0800

    书籍详情的结构编写

--- app/src/main/java/com/design/reader/module/main/fragment/mine/MineFragment.java
+++ app/src/main/java/com/design/reader/module/main/fragment/mine/MineFragment.java
@@ -1,11 +1,42 @@
 package com.design.reader.module.main.fragment.mine;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.design.reader.R;
 import com.design.reader.base.BaseFragment;
+import com.design.reader.module.setting.SettingActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MineFragment extends BaseFragment<MineView, MinePresenter> implements MineView {
+    @BindView(R.id.setting_button)
+    ImageView settingButton;
+    @BindView(R.id.user_name)
+    TextView userName;
+    @BindView(R.id.user_name_layout)
+    LinearLayout userNameLayout;
+    @BindView(R.id.user_phone)
+    TextView userPhone;
+    @BindView(R.id.user_phone_layout)
+    LinearLayout userPhoneLayout;
+    @BindView(R.id.user_state)
+    TextView userState;
+    @BindView(R.id.user_wallet)
+    TextView userWallet;
+    @BindView(R.id.purchased_count)
+    TextView purchasedCount;
+    @BindView(R.id.leased_count)
+    TextView leasedCount;
+
     @Override
     public void initViews(View view) {
 
@@ -15,6 +46,17 @@ public class MineFragment extends BaseFragment<MineView, MinePresenter> implemen
     public int getLayoutId() {
         return R.layout.fragment_mine;
     }
+
+    @OnClick({R.id.setting_button})
+    void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.setting_button:
+                Intent intent = new Intent(getActivity(), SettingActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
 
     @Override
     public MinePresenter createPresenter() {
--- app/src/main/java/com/design/reader/module/setting/SettingActivity.java
+++ app/src/main/java/com/design/reader/module/setting/SettingActivity.java
@@ -1,14 +1,68 @@
 package com.design.reader.module.setting;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.design.reader.R;
 import com.design.reader.base.BaseActivity;
+import com.design.reader.module.AboutActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SettingActivity extends BaseActivity<SettingView, SettingPresenter> implements SettingView {
 
 
+    @BindView(R.id.font_number)
+    TextView fontNumber;
+    @BindView(R.id.font_layout)
+    LinearLayout fontLayout;
+    @BindView(R.id.module)
+    TextView module;
+    @BindView(R.id.module_layout)
+    LinearLayout moduleLayout;
+    @BindView(R.id.about_layout)
+    LinearLayout aboutLayout;
+    @BindView(R.id.version_number)
+    TextView versionNumber;
+    @BindView(R.id.version_layout)
+    LinearLayout versionLayout;
+    @BindView(R.id.logout)
+    Button logout;
+
     @Override
     public void initViews() {
 
+    }
+
+    @OnClick({R.id.logout, R.id.about_layout})
+    void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.logout:
+                new AlertDialog.Builder(this).setTitle("提示").setPositiveButton(getResources().getString(R.string.sure), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).setMessage("确定退出登录?").create().show();
+                break;
+            case R.id.about_layout:
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 
     @Override
